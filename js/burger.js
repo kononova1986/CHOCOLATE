8a6a9c313934b7cf9fec727415b1ee0319232d8b
Екатерина <ironsupergirl@gmail.com>
2024-07-20 20:12:51 +0300

    addmenu

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,19 @@
 const menu = document.querySelector(".wrap-burger");
 const icon = document.querySelector(".burger-icon-btn");
 const iconX = document.querySelector(".icon");
+const items = document.querySelector(".item-burger");
+const how = document.querySelector(".how");
+const chocolate = document.querySelector(".chocolate");
+
+const closeMenu = () => {
+  menu.classList.remove("show-menu");
+};
 
 icon.addEventListener("click", () => {
   menu.classList.add("show-menu");
 });
 
-iconX.addEventListener("click", () => {
-  menu.classList.remove("show-menu");
-});
+iconX.addEventListener("click", closeMenu);
+items.addEventListener("click", closeMenu);
+how.addEventListener("click", closeMenu);
+chocolate.addEventListener("click", closeMenu);
